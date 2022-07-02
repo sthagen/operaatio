9ef2f0dfce1114a67c2bdc1793926d38fc75cf24
Stefan Hagen <stefan@hagen.link>
2022-07-02 19:15:30 +0200

    Added cmake deploy script (WIP)

--- src/deploy-cmake.sh
+++ src/deploy-cmake.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
-# Deploy cppcheck as compiled from a public proxy repository for system wide use
-tool="cppcheck"
+# Deploy cmake as compiled from the public vendor repository for system wide use
+tool="cmake"
 binary="${tool}"
 server_url="https://github.com"
-provider_org="sthagen"
-repo_dir="danmar-${tool}"
+provider_org="Kitware"
+repo_dir="CMake"
+version="3.23.2"
 upstream_repo_url="${server_url}/${provider_org}/${repo_dir}"
 build="${HOME}/d/${tool}"
 prefix="/opt/${tool}"
@@ -16,13 +17,21 @@ sudo mkdir -p "${prefix}" && sudo chown "${USER}":"${USER}" "${prefix}" || exit 
 
 mkdir -p "${build}" && cd "${build}"  || exit 1
 
-[ -d "${build}/${repo_dir}" ] || git clone "${upstream_repo_url}"
+common_base_url="${upstream_repo_url}"/releases/download/v"${version}"/"${tool}"-"${version}"
+curl -OL "${common_base_url}"-SHA-256.txt
+curl -OL "${common_base_url}".tar.gz
+sha256sum -c --ignore-missing "${tool}"-"${version}"-SHA-256.txt || exit 1
 
-cd "${repo_dir}" || exit 1
+kitware_signing_subkey="C6C265324BBEBDC350B513D02D2CEF1034921684"
+curl -OL "${common_base_url}"-SHA-256.txt.asc
+gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys "${kitware_signing_subkey}"
+gpg --verify "${tool}"-"${version}"-SHA-256.txt.asc "${tool}"-"${version}"-SHA-256.txt || exit 1
 
-git pull  && rm -fr build && \
-  cmake -DCMAKE_INSTALL_PREFIX="${prefix}" -S . -B build && \
-  cmake --build build --config Release --target install
+printf "LD Library Path shall contain (%s) and value found is (%s)\n" "/opt/gcc/lib64" "${LD_LIBRARY_PATH}"
+
+tar -xzvf "${tool}"-"${version}".tar.gz && cd "${tool}"-"${version}"/ || exit 1
+./bootstrap -- -DCMAKE_INSTALL_PREFIX:PATH="${prefix}" || exit 1
+make -j$(nproc) && make install || exit 1
 
 if ! grep -q "${profile_token}" "${profile_path}"
 then
@@ -36,6 +45,10 @@ then
 fi
 
 printf "Testing the freshly build and deployed %s using the binary %s ...\n" "${tool}" "${bin_path}/${binary}"
-which "${binary}"
-"${binary}" --help
-"${binary}" --version
+which "${tool}"
+"${bin_path}/${tool}" --version
+ls -lrt "${bin_path}"
+"${bin_path}/ccmake" --version
+"${bin_path}/ctest" --version
+"${bin_path}/cpack" --version
+"${tool}" --version
