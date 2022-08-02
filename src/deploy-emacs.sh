764c98cc5cd6522a2f6cd5015baec0108571df74
Stefan Hagen <stefan@dilettant.eu>
2022-08-02 12:23:38 +0200

    Update deploy-emacs.sh

--- src/deploy-emacs.sh
+++ src/deploy-emacs.sh
@@ -2,11 +2,12 @@
 # Deploy emacs as compiled from a public proxy repository for system wide use
 tool="emacs"
 binary="${tool}"
-server_url="https://ftp.gnu.org/pub/"
+server_url="https://ftp.gnu.org"
 provider_org="gnu"
 repo_dir="${tool}"
-version=${1:28.1}
-upstream_repo_url="${server_url}/${provider_org}/${repo_dir}"
+version=${1:-28.1}
+common_base_url="${server_url}/pub/${provider_org}/${repo_dir}/${tool}-"
+keyring_url="${server_url}/${provider_org}/${provider_org}-keyring.gpg"
 build="${HOME}/d/${tool}"
 prefix="/opt/${tool}"
 bin_path="${prefix}/bin"
@@ -17,14 +18,14 @@ sudo mkdir -p "${prefix}" && sudo chown "${USER}":"${USER}" "${prefix}" || exit 
 
 mkdir -p "${build}" && cd "${build}"  || exit 1
 
-common_base_url="${common_base_url}/${repo_dir}/${tool}-${version}/${tool}-${version}"
-curl -kLO "${common_base_url}".tar.gz
-curl -kLO "${common_base_url}".tar.gz.sig
-curl -kLO "${upstream_repo_url}/${provider_org}"-keyring.gpg
+printf "Installing emacs(%s) from upstream source below (%s) ...\n" "${version}" "${prefix}"
+curl -kLO "${common_base_url}${version}".tar.xz
+curl -kLO "${common_base_url}${version}".tar.xz.sig
+curl -kLO "${keyring_url}"
 
 gpg --verify --keyring ./"${provider_org}"-keyring.gpg "${tool}-${version}.tar.xz.sig" || exit 1
 
-tar -axvf "${tool}-${version}".tar.xz && cd "${tool}-${version}" || exit 1
+tar -axf "${tool}-${version}".tar.xz && cd "${tool}-${version}" || exit 1
 
 # sudo apt install libxpm-dev libjpeg-dev libgif-dev libtiff-dev libcairo-dev libharfbuzz-dev
 
@@ -40,7 +41,6 @@ then
         printf "%s\n" "${profile_token}"
         printf "# -+-\n"
     } >> "${profile_path}"
-    $SHELL
 fi
 
 printf "Testing the freshly build and deployed %s using the binary %s ...\n" "${tool}" "${bin_path}/${binary}"
