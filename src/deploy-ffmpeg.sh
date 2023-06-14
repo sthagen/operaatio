c2c079ae9905fa62ba9bd25486a5e8bbdaeaa024
Stefan Hagen <stefan@dilettant.eu>
2023-06-14 08:32:51 +0200

    Create deploy-ffmpeg.sh

---
+++ src/deploy-ffmpeg.sh
@@ -0,0 +1,46 @@
+#! /usr/bin/env bash
+# Deploy ffmpeg compiled from source
+tool="ffmpeg"
+binary="${tool}"
+server_url="https://github.com"
+provider_org="sthagen"
+repo_dir="FFmpeg-FFmpeg"
+upstream_repo_url="${server_url}/${provider_org}/${repo_dir}"
+prefix="/opt/${tool}"
+bin_path="${prefix}/bin"
+src_path="${prefix}/src"
+profile_path="${HOME}"/.bashrc
+profile_token="export PATH=${bin_path}:\$PATH"
+
+sudo mkdir -p "${prefix}" && sudo chown "${USER}":"${USER}" "${prefix}" || exit 1
+sudo mkdir -p "${src_path}" && sudo chown "${USER}":"${USER}" "${src_path}" && cd "${src_path}"  || exit 1
+
+[ -d "${src_path}/${repo_dir}" ] || git clone "${upstream_repo_url}"
+
+cd "${repo_dir}" || exit 1
+
+git pull --rebase && \
+  ./configure --prefix="${prefix}" && \
+  make && \
+  make install && \
+  make clean
+
+if ! grep -q "${profile_token}" "${profile_path}"
+then
+    printf "Prepending tool bin path %s to PATH by appending to profile %s" "${bin_path}" "${profile_path}"
+    {
+        printf "\n# -+- experimental %s infrastructure\n" "${tool}"
+        printf "%s\n" "${profile_token}"
+        printf "# -+-\n"
+    } >> "${profile_path}"
+    $SHELL
+fi
+
+printf "Testing the freshly build and deployed %s using the binary %s ...\n" "${tool}" "${bin_path}/${binary}"
+which -a "${binary}"
+"${binary}" -version
+file "$(which ${binary})"
+stat "$(which ${binary})"
+sha256sum "$(which ${binary})"
+ssdeep "$(which ${binary})"
+b3sum "$(which ${binary})"
